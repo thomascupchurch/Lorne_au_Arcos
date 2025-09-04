64d34f97672698384106a89ab57984e0936946bb
thomascupchurch <thomascupchurch@gmail.com>
2025-09-04 09:08:51 -0500

    user auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from app.routes import main
-from app.models import db
+from app.models import db, User
 
 def create_app():
     app = Flask(__name__)
@@ -10,5 +11,13 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'main.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     app.register_blueprint(main)
     return app
